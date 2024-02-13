85bb3a11862a378d01ec05e5406db446b8cc2919
Marko Miir <marko@verston.ee>
2024-02-13 11:08:07 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See on kommentaar minu koodile
+
+//rakendus küsib kasutajalt sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Sistesta oma nimi:"); //Output
+//string - sõne
+string kasutajaNimi = Console.ReadLine(); //Input
+
+
+Console.WriteLine("Tere" +", " +  kasutajaNimi + "!"); //Output
+//string interpolation
+
+Console.WriteLine($"Tere, {kasutajaNimi}!"); //Output
+
+
